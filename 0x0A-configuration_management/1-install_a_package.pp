17a0b983a98706bd783e42092203ef53c6e5a5c0
AyaTarek95 <aya.tarek213@icloud.com>
2023-12-22 11:08:01 -0800

    fix1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,5 @@
 # install flask from pip3.
-package { 'python3-pip':
-  ensure => present,
-}
-
-package { 'flask':
+package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Package['python3-pip'],
 }
