23145680c3c156c5e26bd3d1fefae1a0d03d2976
AyaTarek95 <aya.tarek213@icloud.com>
2024-02-13 13:44:42 -0800

    task1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" prints the titles of the first 10
+hot posts listed for a given subreddit"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """ prints the titles of the first 10 hot posts
+    listed for a given subreddit"""
+
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+    res = requests.get(url, headers={'User-Agent': 'AyaTarek95'},
+                       allow_redirects=False)
+
+    if res.status_code == 200:
+        posts = res.json().get('data', {}).get('children', [])
+        for post in posts:
+            print(post.get('data', {}).get('title', ''))
+    else:
+        print(None)
