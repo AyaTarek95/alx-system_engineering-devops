8b3851f5a2759ef92d147b48243a5e36b4031077
AyaTarek95 <aya.tarek213@icloud.com>
2024-02-14 08:43:13 -0800

    task2

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Using recrsion to get title of top 10 hot posts"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after="", count=0):
+    """Return list of titles for all hot posts on 
+    subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    headers = {"User-Agent": "AyaTarek95"}
+    response = requests.get(url, headers=headers,
+                            params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        return None
+
+    results = response.json().get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        hot_list.append(c.get("data").get("title"))
+
+    if after is not None:
+        return recurse(subreddit, hot_list, after, count)
+    return hot_list
