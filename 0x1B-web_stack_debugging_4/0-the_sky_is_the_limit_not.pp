ad5386d0208563e902b9ccf445d3c80b6db2814a
AyaTarek95 <aya.tarek213@icloud.com>
2024-02-22 12:47:22 -0800

    task0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,10 @@
+exec {'fix-ulimit':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
+}
+
+exec {'restart-nginx':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+}
