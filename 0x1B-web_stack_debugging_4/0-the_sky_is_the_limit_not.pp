f1937073966a37821e874fddbc3221ac7fc109ef
AyaTarek95 <aya.tarek213@icloud.com>
2024-02-23 10:15:57 +0000

    task0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,11 @@
-exec {'fix-ulimit':
-  provider => shell,
-  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
-  before   => Exec['restart'],
+# Increase ULIMIT of the default file
+exec { 'fix--for-nginx':
+  command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/',
 }
-
-exec {'restart-nginx':
-  provider => shell,
-  command  => 'sudo service nginx restart',
+#->
+# Restart Nginx
+exec { 'nginx-restart':
+  command => '/etc/init.d/nginx restart',
+  path    => '/etc/init.d/',
 }
